11b42d8ee89842073b792649f953462f9d70a7af
henriquemattia <mattia.henrique@gmail.com>
2022-12-16 16:33:48 -0300

    Adicionando mais testes ao content

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -2,4 +2,14 @@ import { Content } from './content';
 
 test('It be should be able to create a notification content', () => {
   const content = new Content('Você recebeu uma solicitação de amizade');
+
+  expect(content).toBeTruthy();
+});
+
+test('It should not be able to create a notification content with less then 5 characters', () => {
+  expect(() => new Content('aaa')).toThrow();
+});
+
+test('It should not be able to create a notification content with more then 240 characters', () => {
+  expect(() => new Content('a'.repeat(241))).toThrow();
 });
