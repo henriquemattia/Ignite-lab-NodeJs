436f299be5ded8a998d5492e8b5b1d4f466ccdc6
henriquemattia <mattia.henrique@gmail.com>
2022-12-20 18:01:07 -0300

    Teste

--- src/application/entities/notification.spec.ts
+++ src/application/entities/notification.spec.ts
@@ -1,9 +1,10 @@
 import { Content } from './content';
+import { Notification } from './notification';
 
 describe('Notification', () => {
   it('should be able to create a notification', () => {
     const notification = new Notification({
-      content: Content('Nova soliciatação de amizade'),
+      content: new Content('Nova soliciatação de amizade'),
       category: 'social',
       recipientId: 'Exemple',
     });
